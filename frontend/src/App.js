2c45048e279cc4baa442390fe8555921b69c66ad
Majid <nastaran.samui@gmail.com>
2021-04-29 15:09:59 +0700

    route fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,13 @@ function App() {
     <div className="grid-container">
       <header className="row">
         <div>
-          <a className="brand" href="index.html">
+          <a className="brand" href="/">
             ECommerce
           </a>
         </div>
         <div>
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
         </div>
       </header>
       <main>
@@ -21,7 +21,7 @@ function App() {
                 <img className="medium" src={product.image} alt={product.name} />
               </a>
               <div className="card-body">
-                <a href="product.html">
+              <a href={`/product/${product._id}`}>
                   <h2>{product.name}</h2>
                 </a>
                 <div className="rating">
