68e4d3bc9db1df6c49d050eb5058435902b308e6
Majid <nastaran.samui@gmail.com>
2021-04-29 15:05:44 +0700

    create React app

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+  products:[
+    {
+      _id: 0,
+      name: 'Nike Slim Shirt',
+      category: "Shirts",
+      image: '/images/p0.jpg',
+      price: 120,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product'
+    },
+    {
+      _id: 1,
+      name: 'Adidas Slim Shirt',
+      category: "Shirts",
+      image: '/images/p1.jpg',
+      price: 100,
+      brand: "Adidas",
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product'
+    },
+    {
+      _id: 2,
+      name: 'Lacoste Free Shirt',
+      category: "Shirts",
+      image: '/images/p2.jpg',
+      price: 220,
+      brand: "Lacoste",
+      rating: 4.8,
+      numReviews: 10,
+      description: 'high quality product'
+    },
+    {
+      _id: 3,
+      name: 'Nike Slim Pants',
+      category: "Pants",
+      image: '/images/p3.jpg',
+      price: 78,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product'
+    },
+    {
+      _id: 4 ,
+      name: 'Puma Slim Pants',
+      category: "Pants",
+      image: '/images/p4.jpg',
+      price: 60,
+      brand: "Puma",
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product'
+    },
+    {
+      _id: 5 ,
+      name: 'Adidas Slim Pants',
+      category: "Pants",
+      image: '/images/p5.jpg',
+      price: 139,
+      brand: "Adidas",
+      rating: 4.5,
+      numReviews: 15,
+      description: 'high quality product'
+    },
+  ]
+}
+
+export default data;
