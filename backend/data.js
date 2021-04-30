ba36b93984f30732a305a0109db2e9f6eeccf21d
Majid <nastaran.samui@gmail.com>
2021-04-30 11:18:24 +0700

    add redux and backend product details

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data = {
+  products:[
+    {
+      _id: 0,
+      name: 'Nike Slim Shirt',
+      category: "Shirts",
+      image: '/images/p0.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product'
+    },
+    {
+      _id: 1,
+      name: 'Adidas Slim Shirt',
+      category: "Shirts",
+      image: '/images/p1.jpg',
+      price: 100,
+      countInStock: 20,
+      brand: "Adidas",
+      rating: 1.5,
+      numReviews: 10,
+      description: 'high quality product'
+    },
+    {
+      _id: 2,
+      name: 'Lacoste Free Shirt',
+      category: "Shirts",
+      image: '/images/p2.jpg',
+      price: 220,
+      countInStock: 0,
+      brand: "Lacoste",
+      rating: 2.8,
+      numReviews: 10,
+      description: 'high quality product'
+    },
+    {
+      _id: 3,
+      name: 'Nike Slim Pants',
+      category: "Pants",
+      image: '/images/p3.jpg',
+      price: 78,
+      countInStock: 15,
+      brand: "Nike",
+      rating: 3.5,
+      numReviews: 14,
+      description: 'high quality product'
+    },
+    {
+      _id: 4 ,
+      name: 'Puma Slim Pants',
+      category: "Pants",
+      image: '/images/p4.jpg',
+      price: 60,
+      countInStock: 5,
+      brand: "Puma",
+      rating: 4,
+      numReviews: 10,
+      description: 'high quality product'
+    },
+    {
+      _id: 5 ,
+      name: 'Adidas Slim Pants',
+      category: "Pants",
+      image: '/images/p5.jpg',
+      price: 139,
+      countInStock: 12,
+      brand: "Adidas",
+      rating: 5,
+      numReviews: 15,
+      description: 'high quality product'
+    },
+  ]
+}
+
+export default data;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,40 @@
-import React from 'react';
-import data from '../data';
-import Product from '../components/Product'
+import React, { useEffect } from "react";
+import Product from "../components/Product";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
+
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
+  useEffect(() => {
+    let isMount = true;
+    if (isMount) {
+      dispatch(listProducts());
+    }
+    return () => {
+      isMount = false;
+    };
+  }, [dispatch]);
+
   return (
-    <div className="row center">
-          {data.products.map((product) => (
-            <Product key={product._id} product={product}/>
+    <div>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {products.map((product) => (
+            <Product key={product._id} product={product} />
           ))}
         </div>
+      )}
+    </div>
   );
-}
+};
 
 export default HomeScreen;
